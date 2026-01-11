59831beeea5c3b96cdd6225f47615d2329976df8
Josh <jxsh@hey.com>
2026-01-11 06:45:58 +0530

    initial setup

---
+++ Day-1-hello-rust/target/debug/Day-1-hello-rust.d
@@ -0,0 +1,1 @@
+/Users/jxsh/Desktop/100-Days-of-Rust/Day-1-hello-rust/target/debug/Day-1-hello-rust: /Users/jxsh/Desktop/100-Days-of-Rust/Day-1-hello-rust/src/main.rs
---
+++ Day-1-hello-rust/target/debug/deps/Day_1_hello_rust-1f44fe7f73ca7238.d
@@ -0,0 +1,5 @@
+/Users/jxsh/Desktop/100-Days-of-Rust/Day-1-hello-rust/target/debug/deps/Day_1_hello_rust-1f44fe7f73ca7238.d: src/main.rs
+
+/Users/jxsh/Desktop/100-Days-of-Rust/Day-1-hello-rust/target/debug/deps/Day_1_hello_rust-1f44fe7f73ca7238: src/main.rs
+
+src/main.rs:
